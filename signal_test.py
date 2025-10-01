bbd24b99376b1669c76b95b1bb3fd4f68deb4555
putic <you@example.com>
2025-10-01 10:05:21 +0300

    добавлены вычисления амплитуды и длительности сигнала

--- signal_test.py
+++ signal_test.py
@@ -1,25 +1,23 @@
 
-###########################################################
-
 import sys  
 import os
 from PyQt5 import QtWidgets, uic
 import pyqtgraph as pg
 import numpy as np
 
-from PyQt5.QtCore    import * #Qt #* # импортировать все классы QtCore
-from PyQt5.QtGui     import * # импортировать все классы QtGui
-from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow, QWidget, QPushButton, QListWidget, QSplitter, QVBoxLayout, QLabel, QLineEdit, QMessageBox #* # импортировать все классы QtWidget
+from PyQt5.QtCore    import * 
+from PyQt5.QtGui     import * 
+from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow, QWidget, QPushButton, QListWidget, QSplitter, QVBoxLayout, QLabel, QLineEdit, QMessageBox
 
 from scipy.signal import savgol_filter
+import scipy.signal as signal_s
+from shapely.geometry import LineString
 
 
 y2 = []
 y = []
 x = []
 
-###################################################
-#class CustomDialog(QtWidgets.QDialog):
 class CustomDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -57,17 +55,17 @@ class CustomDialog(QtWidgets.QDialog):
         self.setLayout(layout)
 
     def get_win(self):
-        return self.win #self.name_input.text()
+        return self.win
 
     def get_poly(self):
-        return self.poly #self.name_input.text()
+        return self.poly
         
     def get_result(self):
-        return self.result #self.name_input.text(
+        return self.result
         
     def push_cancel(self):
-        self.result=5#0
-        self.close()#done(QDialog.Rejected)
+        self.result=0
+        self.close()
 
     def push_ok(self):
         self.result = 2
@@ -75,7 +73,6 @@ class CustomDialog(QtWidgets.QDialog):
             win = int(self.win_input.text())
             poly = int(self.poly_input.text())
             if(poly<win):
-                #print(win, poly)
                 self.win = win
                 self.poly = poly
                 self.result=1    
@@ -83,7 +80,7 @@ class CustomDialog(QtWidgets.QDialog):
             print(f"'{string_value}'FATAL ERROR: is not a valid integer.")
             exit(1)
         
-        self.close()#done(QDialog.Accepted)
+        self.close()
 
     def closeEvent(self, event: QCloseEvent):
         if(self.result==2):
@@ -98,71 +95,75 @@ class CustomDialog(QtWidgets.QDialog):
     def ok(self):
         self.poly= edit1.text.asint()
         self.win=edit2.text.asint()    
-############################################
 
-class MainWindow(QtWidgets.QMainWindow): # класс окна от базового класса QMainWindow
+class MainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self, *args, **kwargs): # конструктор класса
+    def __init__(self, *args, **kwargs):
         
         super(MainWindow, self).__init__(*args, **kwargs) # вызвать конструктор базового класса QMainWindow
 
         self.calcPlot = None
         
-        #uic.loadUi('win.ui', self) # Загрузите файл описания интерфейса на QT в интерфейс окна (win.ui сздан в Qt Designer)
-        
-        #self.setGeometry(300, 300, 300, 150) # установить размер окна по умолчанию (можно не делать, потомучто потом окно максимизируется)
-        
-        self.centralwidget = QWidget() # создаем центральную обласьб окна, в которой будут все элементы
+        self.centralwidget = QWidget()
         self.GraphWidget = pg.PlotWidget()
+        self.GraphWidgetResult = pg.PlotWidget()
         self.BFile = QPushButton('Файл')
         self.BCalc = QPushButton('Рассчитать')
         self.BClear = QPushButton('Очистить')
         self.listResult = QListWidget()
+        self.listResult.setGeometry(0,0,500,200)
         self.setCentralWidget(self.centralwidget)
         
         self.BCalc.setEnabled(False)
         self.BClear.setEnabled(False)
 
-        self.setWindowTitle("Расчет сигнала") # установить название окна
+        self.setWindowTitle("Расчет сигнала")
 
-        grid = QtWidgets.QGridLayout(self.centralwidget) # создать табличный компановщик размещения элементов окна как основной центральный элемент окна
-        grid.addWidget(self.BFile, 0, 0) # добавить в компановщик объект кнопки BFile, определенный в файла win.ui по координатам 0,0 (первая строка, первый столбец таблицы компановщика grid)
+        grid = QtWidgets.QGridLayout(self.centralwidget)
+        grid.addWidget(self.BFile, 0, 0)
         grid.addWidget(self.BCalc, 1, 0) 
         grid.addWidget(self.BClear, 2, 0) 
         
-        wg = QWidget() # создать объект пустого виджета
-        grid.addWidget(wg, 3, 0) # добавить пустой виджет в третью строку первога столбца табличного компановщика grid
+        wg = QWidget()
+        grid.addWidget(wg, 3, 0)
 
-        split = QSplitter(Qt.Vertical) # создать объект сплиттер разделяющий окна по вертикали для изменения их размеров
-        split.addWidget(self.GraphWidget) # добавить верхним элементом в сплиттер объект отображающий график GraphWidget, взятый из win.ui
-        split.addWidget(self.listResult) # добавить нижним элементом в сплиттер объект отображающий результаты расчетов listResult, взятый из win.ui
-        #split.addWidget(QPushButton('rrr'))
-        split.setSizes([800, 200]) # установить пропорции элементов в сплиттере
+        split = QSplitter(Qt.Vertical)
+        split.addWidget(self.GraphWidget)
+        split.addWidget(self.GraphWidgetResult)
+        split.addWidget(self.listResult)
         
-        grid2 = QtWidgets.QGridLayout(wg) # создать табличный компановщик размещения элементов в пустом виджете wg
-        grid2.addWidget(split) # добавить сплиттер со всеми ранее добавленными в него элементами в компановщик grid2
-        #grid2.addWidget(QPushButton('qqq'))
+        grid2 = QtWidgets.QGridLayout(wg)
+        grid2.addWidget(split)
 
+        self.GraphWidget.clear()
+        self.GraphWidget.setBackground("k")
 
-        self.GraphWidget.clear() # очистить график
-        self.GraphWidget.setBackground("k") # установить черный фон графика
+        self.GraphWidget.addLegend()
+        self.GraphWidget.setLabel(axis='left', text='V, В')
+        self.GraphWidget.setLabel(axis='bottom', text='t, ns')
+        self.GraphWidget.getAxis('left').setTextPen('y')
+        self.GraphWidget.getAxis('bottom').setTextPen('y')
 
-        self.GraphWidget.addLegend() # разрешить отображать легенду на графике
-        self.GraphWidget.setLabel(axis='left', text='V, В') # установить название оси Y на графике
-        self.GraphWidget.setLabel(axis='bottom', text='t, ns') # установить название оси X на графике
-        self.GraphWidget.getAxis('left').setTextPen('y') # установить желтый цвет подписи название оси Y на графике
-        self.GraphWidget.getAxis('bottom').setTextPen('y') # установить желтый цвет подписи название оси X на графике
+        self.GraphWidgetResult.addLegend()
+        self.GraphWidgetResult.setLabel(axis='left', text='V, В')
+        self.GraphWidgetResult.setLabel(axis='bottom', text='t, ns')
+        self.GraphWidgetResult.getAxis('left').setTextPen('y')
+        self.GraphWidgetResult.getAxis('bottom').setTextPen('y')
 
-        self.BFile.clicked.connect(self.get_file) # установить функцию get_file как обработчик по нажатию на кнопку объекта BFile
-        self.BCalc.clicked.connect(self.calc) # 
-        self.BClear.clicked.connect(self.clear) #
+        self.BFile.clicked.connect(self.get_file)
+        self.BCalc.clicked.connect(self.calc) 
+        self.BClear.clicked.connect(self.clear)
             
-        self.showMaximized() # сделать окно на весь экран
+        self.showMaximized()
 
-    def plot(self, x, y, desc, color): # функция класса MainWindow, рисующая график (desc - имя файла с данными)
-        p = self.GraphWidget.plot(x, y,pen=color, name=desc) # нарисовать график заданным цветом (pen=color), и отобразить легенду в виде строки desc
+    def plot(self, x, y, desc, color): 
+        p = self.GraphWidget.plot(x, y,pen=color, name=desc)
         return p
     
+    '''def hlines(self, lines, clr):
+        p = self.GraphWidget.addLine(lines, color=clr)
+        return p
+    '''
     def clear(self):
         global y2
         global y
@@ -170,6 +171,7 @@ class MainWindow(QtWidgets.QMainWindow): # класс окна от базово
 
         if (self.calcPlot != None):
             self.GraphWidget.removeItem(self.calcPlot)
+            self.GraphWidgetResult.clear()
             self.calcPlot = None
             self.BClear.setEnabled(False)
             self.listResult.clear()
@@ -185,46 +187,57 @@ class MainWindow(QtWidgets.QMainWindow): # класс окна от базово
 
             if self.calcPlot != None:
                 self.GraphWidget.removeItem(self.calcPlot)
+                self.GraphWidgetResult.clear()
                 self.calcPlot = None
+                self.BClear.setEnabled(False)
 
-############################################
-        dlg = CustomDialog()
-        dlg.exec_()
-        
-        if (dlg.get_result() == 1):
-            win = dlg.get_win()
-            poly = dlg.get_poly()
+
+            dlg = CustomDialog()
+            dlg.exec_()
+
+            if (dlg.get_result() == 1):
+                win = dlg.get_win()
+                poly = dlg.get_poly()
+                    
+                y2 = savgol_filter(y, win, poly) 
+                legend_str = "Сглаженный сигнал: win= " + str(win) + " poly= " + str(poly)
+                self.calcPlot = self.plot(x, y2, legend_str,'w') 
+
+                zz, _= signal_s.find_peaks(y)
+
+                self.GraphWidgetResult.clear()
+                self.GraphWidgetResult.plot(x[zz], y[zz], pen='r', name="Пиковый сигнал")
+                y4 = [] # амплитуда
+                sko = np.std(y[zz]) 
+                y3 = [] # среднее
+                for t in range(len(y[zz])):
+                    y3.append(sko)
+                    y4.append(sko*2)
+                self.GraphWidgetResult.plot(x[zz], y3, pen='y', name="Среднее")
                 
-            y2 = savgol_filter(y, win, poly) # массив значений сглаженной функции, скоторыми можно делать рассчеты параметров
-            legend_str = "Рассчетный сигнал: win= " + str(win) + " poly= " + str(poly)
-            self.calcPlot = self.plot(x, y2, legend_str,'w') # вызвать функцию класса, которая нарисует график
                 
-            self.listResult.addItem("Va = 10 В") # добавить результат расчета параметра
-            self.listResult.addItem("Vb = 5 В") # добавить результат расчета параметра
-############################################                
-            
-            # win и poly - параметры функции сглаживания  savgol_filter надо подобрать. Сделать диалог с возможностью задавать эти параметры
-            #dp= MayDialog()
-            #dp.show()
-            '''
-            win = 11 # значение параметра окна сглаживания
-            poly = 3 # значение степени полинома сглаживания (poly < win)
+                self.GraphWidgetResult.plot(x[zz], y4, pen='g', name="Амплитуда")
 
-            y2 = savgol_filter(y, win, poly) # массив значений сглаженной функции, скоторыми можно делать рассчеты параметров
-            self.calcPlot = self.plot(x, y2, "Рассчетный сигнал",'w') # вызвать функцию класса, которая нарисует график
-            
-            self.listResult.addItem("Va = 10 В") # добавить результат расчета параметра
-            self.listResult.addItem("Vb = 5 В") # добавить результат расчета параметра
-            '''
-            self.GraphWidget.removeItem(dlg)
+                
+                t1  = np.max(y4)
+                self.listResult.addItem("Амплитуда: "+"{:.2f}".format(t1)+" В")    
+                
+                first_line = LineString(np.column_stack((x[zz], y[zz])))
+                second_line = LineString(np.column_stack((x[zz], y3)))
+                intersection = first_line.intersection(second_line) #координаты точек пересечения красного и среднего
+                #print(intersection)
+                points = [p for p in intersection.geoms]
+                #print(points)
+                
+                ps = [item[1] for item in sorted([(pt.x,pt) for pt in points])] # сортировка по X
+                #print(ps)
 
-            self.BCalc.setEnabled(True)
-            self.BClear.setEnabled(True)
-        if (dlg.get_result() == 5):
-            self.BClear.setEnabled(False)
+                self.listResult.addItem("T: "+"{:.2f}".format(ps[1].x - ps[0].x)+" ns") 
+                
+                self.BCalc.setEnabled(True)
+                self.BClear.setEnabled(True)
 
-
-    def get_file(self): # функция класса MainWindow, вызываемая при нажатии на кнопку BFile
+    def get_file(self):
 
         global x
         global y
@@ -232,12 +245,12 @@ class MainWindow(QtWidgets.QMainWindow): # класс окна от базово
         try:
 
             file_name, _ = QFileDialog.getOpenFileName(self, 'Signal Data', r"", "") # открыть диалог выбора файла, по закрытию диалога в переменную file_name возвратится полное имя файла
-            if os.path.exists(file_name):  # проверить, что файл существует            
-                file = open(file_name,'r') # открыть файл в переменную file
+            if os.path.exists(file_name):              
+                file = open(file_name,'r') 
             
-                content_y = file.read().replace('[','').replace(']','').replace(' ','').split(",") # считать данные из файла в массив строку удалив символы "[] ", где запятая - разделитель между элементами массива
+                content_y = file.read().replace('[','').replace(']','').replace(' ','').split(",") 
                 a = content_y
-                y = np.asarray(a, dtype=float) # сформиовать массив Y, как массив из чисел с плавающей запятой
+                y = np.asarray(a, dtype=float) 
         except OSError:
             print("Ошибка OSError")
         except TypeError:
@@ -245,33 +258,34 @@ class MainWindow(QtWidgets.QMainWindow): # класс окна от базово
         except ValueError:
             print("Недопустимое значение. Невозможно считать данные из файла!")
         except IndexError:
-            print("Ошибка IndexError")
+            print("Огшибка IndexError")
         except ZeroDivisionError:
             print("Деление на ноль")
         except FileNotFoundError:
             print("Файл не найден.")
         else:
 
-            tmp = 0 # значение первого элемента массива y
-            x = [] # пустой массив X
-            for t in range(len(y)): # в цикле от 0 до числа элементов в массиве y-1
-                x.append(tmp) # добавить элемент в массив x
-                tmp = tmp + 50 # сформиовать очередное значение элемента массива x
+            tmp = 0 
+            x = [] 
             
-            self.listResult.clear()  # очистить результаты расчетов
-            self.GraphWidget.clear() # очистить график
-            self.plot(x, y, file_name,'g') # вызвать функцию класса, которая нарисует график
+            for t in range(len(y)): 
+                x.append(tmp) 
+                tmp = tmp + 50 
+            x = np.array(x)
+            self.listResult.clear()  
+            self.GraphWidget.clear() 
+            self.GraphWidgetResult.clear()
+            self.plot(x, y, file_name,'g') 
             self.calcPlot = None
        
             self.BCalc.setEnabled(True)
             self.BClear.setEnabled(False)
 
-            
+         
 
-# Функция main - основная старт программы
 if __name__ == '__main__': 
-    app = QtWidgets.QApplication(sys.argv) # создать объект программы
-    main = MainWindow() # создать объект окна класса MainWindow
-    main.show() # отобразить  окно
-    sys.exit(app.exec_()) # запустить объект программы, чтобы обрабатывались кнопки и события
+    app = QtWidgets.QApplication(sys.argv) 
+    main = MainWindow() 
+    main.show() 
+    sys.exit(app.exec_()) 
 
